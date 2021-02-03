4c8135021da4866a40adc834f4de03ebde0f9253
matthiashaefeli <haefeli.matthias2gmail.com>
2021-02-03 15:47:55 -0600

    blog crud

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :users
   resources :books
+  resources :blogs
   get '/sessions/new', to: 'sessions#new'
   post '/sessions/create', to: 'sessions#create'
   delete '/sessions/destroy', to: 'sessions#destroy'
