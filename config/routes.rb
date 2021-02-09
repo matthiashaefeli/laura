57fb1c6a34fd0e83375514aa8b912a86358d5a73
matthiashaefeli <haefeli.matthias2gmail.com>
2021-02-09 14:47:00 -0600

    create comment for book works

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :books
   resources :blogs
+  resources :comments
   get '/sessions/new', to: 'sessions#new'
   post '/sessions/create', to: 'sessions#create'
   delete '/sessions/destroy', to: 'sessions#destroy'
